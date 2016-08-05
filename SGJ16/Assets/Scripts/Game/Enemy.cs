30c2246387030cc34a2e9709b627871dfe7b5296
mikolaj <mikolajqc@gmail.com>
2016-08-05 22:53:00 +0200

    fixed problem with enemy

---
+++ SGJ16/Assets/Scripts/Game/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.Ma
+
+namespace Assets.Scripts.Game
+{
+    public class Enemy : MonoBehaviour
+    {
+
+        Movable enemyMovable;
+
+        public Vector3 directPosition = new Vector3(5, 0, -1);
+
+        // Use this for initialization
+        void Start()
+        {
+            enemyMovable = gameObject.GetComponent<Movable>();
+            Debug.Log("enemy spawned");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!enemyMovable.IsExecutingPastMovements())
+            {
+                UpdatePosition();
+            }
+        }
+
+        void UpdatePosition()
+        {
+                float dX = gameObject.transform.position.x - directPosition.x;
+                float dY = gameObject.transform.position.y - directPosition.y;
+
+                if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)) && (gameObject.transform.position != directPosition))
+                {
+                    if ((UnityEngine.Mathf.Abs(dX) > UnityEngine.Mathf.Abs(dY)) && dX != 0)
+                    {
+                        if (dX > 0)
+                        {
+                            enemyMovable.position.x -= 1;
+                        }
+                        else
+                        {
+                            enemyMovable.position.x += 1;
+                        }
+                    }
+                    else if (dY != 0)
+                    {
+                        if (dY > 0)
+                        {
+                            enemyMovable.position.y -= 1;
+                        }
+                        else
+                        {
+                            enemyMovable.position.y += 1;
+                        }
+                    }
+
+                }
+         }
+        
+
+    }
+
+}

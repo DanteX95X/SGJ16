c737b2ea8043bc067d1fb814490122c20fe39d45
Dante <daniellewinski95@gmail.com>
2016-08-06 17:52:55 +0200

    Game state.

---
+++ SGJ16/Assets/Scripts/States/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Assets.Scripts.Game;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.States
+{
+    class Game : State
+    {
+        public override void Init()
+        {
+            Grid.isTimeFlowing = true;
+        }
+
+        public override void UpdateLoop()
+        {
+            if (!Grid.isAnyEnemyAlive)
+            {
+                Debug.Log("Level won");
+                ChangeState<CleanUp>();
+            }
+        }
+    }
+}

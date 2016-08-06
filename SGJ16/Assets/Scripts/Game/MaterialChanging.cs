d3696242bcb522dcd41ea7491c56d69c1f82e8f9
mikolaj <mikolajqc@gmail.com>
2016-08-06 18:24:15 +0200

    sj]fsdsd

---
+++ SGJ16/Assets/Scripts/Game/MaterialChanging.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialChanging : MonoBehaviour {
+
+
+    [SerializeField]
+    Material material = null;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Changer()
+    {
+        GetComponent<Renderer>().material = material;
+    }
+}

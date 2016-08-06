c574b2521f65ac7a4d6ca0dd99fb7d5aded63026
Dante <daniellewinski95@gmail.com>
2016-08-06 14:48:55 +0200

    Field file.

---
+++ SGJ16/Assets/Scripts/Game/Field.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    class Field : MonoBehaviour
+    {
+        public enum FieldType
+        {
+            CLEAR,
+            LOST_TURN,
+            DOUBLE_MOVEMENT,
+        }
+
+        FieldType type;
+
+        public void SetFieldType(FieldType type)
+        {
+            this.type = type;
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void DoAction(Movable movable)
+        {
+            switch(type)
+            {
+                case FieldType.LOST_TURN:
+                    movable.movementPoints = 0;
+                    //Debug.Log(movable.movementPoints);
+                    break;
+                case FieldType.DOUBLE_MOVEMENT:
+                    movable.movementPoints = 2;
+                    break;
+            }
+        }
+    }
+}

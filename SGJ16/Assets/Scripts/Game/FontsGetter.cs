7c77330c3021b3e37e9ffe4e7eb3f6c8009a86b0
mikolaj <mikolajqc@gmail.com>
2016-08-07 05:14:32 +0200

    cjsaj

---
+++ SGJ16/Assets/Scripts/Game/FontsGetter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FontsGetter : MonoBehaviour {
+
+    public Font font = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Font getFont()
+    {
+        return font;
+    } 
+}

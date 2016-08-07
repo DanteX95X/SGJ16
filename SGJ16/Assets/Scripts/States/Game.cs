cce6a4256de029bdc68c3c83960a7ac9a2d5494b
mikolaj <mikolajqc@gmail.com>
2016-08-07 04:55:13 +0200

    Gui and wooden background

--- SGJ16/Assets/Scripts/States/Game.cs
+++ SGJ16/Assets/Scripts/States/Game.cs
@@ -10,13 +10,28 @@ namespace Assets.Scripts.States
 {
     class Game : State
     {
+
+        public double BUTTONWIDTH;
+        public double BUTTONHEIGHT;
+        public double FIRSTLABELPOSX;
+        public double FIRSTLABELPOSY;
+        public double FONTSIZE;
+
         public override void Init()
         {
             Grid.isTimeFlowing = true;
+            FIRSTLABELPOSX = 0.05 * Screen.width;
+            FIRSTLABELPOSY = 0.05 * Screen.height;
+            FONTSIZE = 0.1 * Screen.height;
+
         }
 
         public override void UpdateLoop()
         {
+            FIRSTLABELPOSX = 0.05 * Screen.width;
+            FIRSTLABELPOSY = 0.05 * Screen.height;
+            FONTSIZE = 0.06 * Screen.height;
+
             if (!Grid.isAnyEnemyAlive)
             {
                 Debug.Log("Level won");
@@ -28,15 +43,25 @@ namespace Assets.Scripts.States
             {
                 Debug.Log("Level lost");
                 ChangeState<CleanUp>();
-
             }
-
 
             if (Input.GetKeyDown(KeyCode.Escape) && Grid.isTimeFlowing)
             {
                 ChangeState<Pause>();
 
             }
+        }
+
+        public void OnGUI()
+        {
+            GUIStyle style = new GUIStyle();// = GUIStyle.none;
+            style.fontSize = (int)(FONTSIZE);
+            style.font = GetComponent<FontsGetter>().font;
+
+
+            GUI.Label(new Rect((float)FIRSTLABELPOSX, (float)FIRSTLABELPOSY, 100, 20), "Level: " + (LevelManager.currentLevel + 1), style);
+
+            GUI.Label(new Rect((float)FIRSTLABELPOSX, 1.2f*(float)FIRSTLABELPOSY + (float)FONTSIZE, 100, 20), "Lifes: " + (Grid.lifes + 1), style);
         }
     }
 

da6f30f70f8497be597da4b9462ba453910eade8
Dante <daniellewinski95@gmail.com>
2016-08-07 05:26:08 +0200

    Merge fix.

--- SGJ16/Assets/Scripts/Game/LevelManager.cs
+++ SGJ16/Assets/Scripts/Game/LevelManager.cs
@@ -8,6 +8,6 @@ namespace Assets.Scripts.Game
     class LevelManager
     {
         public static string levelPath = "Levels\\";
-        public static int currentLevel = 2;
+        public static int currentLevel = 0;
     }
 }

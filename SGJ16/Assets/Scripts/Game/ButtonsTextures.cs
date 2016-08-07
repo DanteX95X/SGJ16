b81c8fff0d9e07877937400a3bdd918d290c5901
mikolaj <mikolajqc@gmail.com>
2016-08-07 02:43:56 +0200

    d

---
+++ SGJ16/Assets/Scripts/Game/ButtonsTextures.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ButtonsTextures : MonoBehaviour {
+
+    [SerializeField]
+    public Texture[] buttonTextures = null;
+    public Texture[] buttonTexturesActive = null;    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Texture GetTexture(int i)
+    {
+        return buttonTextures[i];
+    }
+
+    public Texture GetTextureActive(int i)
+    {
+        return buttonTexturesActive[i];
+    }
+}
